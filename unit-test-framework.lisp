ebb82fdac07148dc1b5746add54e6ccce0bbee15
AUTH\rubio <salvador.rubio@between.tech>
2018-11-12 10:27:29 +0100

    chanage loop with mapcar

--- unit-test-framework.lisp
+++ unit-test-framework.lisp
@@ -6,16 +6,19 @@
 
 (defmacro check (&body forms)
   `(combine-results
-     ,@(loop for f in forms collect `(report-result ,f ',f ))))
+     ,@(mapcar #'(lambda (f) `(report-result ,f ',f )) forms)))
+     ;;,@(loop for f in forms collect `(report-result ,f ',f ))))
 
 (defmacro with-gensyms ((&rest names) &body body)
-  `(let ,(loop for n in names collect `(,n (gensym)))
+  `(let ,(mapcar #'(lambda (n) `(,n (gensym))) names)
+  ;;`(let ,(loop for n in names collect `(,n (gensym)))
      ,@body))
 
 (defmacro combine-results (&body forms)
   (with-gensyms (result)
     `(let ((,result t))
-       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,@(mapcar #'(lambda (f) `(unless ,f (setf ,result nil))) forms)
+       ;;,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
        ,result)))
 
 (defmacro deftest (name parameters &body body)
